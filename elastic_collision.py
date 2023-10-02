31481fd2ef1df8cb8dd1a19e3aa72ce879759626
Pranjal Srivastava <143828394+Pranjal-Srivastava-2023@users.noreply.github.com>
2023-10-02 10:52:14 -0400

    two body elastic collision using map and list comprehension

--- elastic_collision.py
+++ elastic_collision.py
@@ -1,70 +1,63 @@
 import numpy as np
+
 def conserve_momentum(m1, v1, m2, v2):
-  """Calculates the final velocities of two bodies in an elastic collision, given their masses and initial velocities.
+    """Calculates the final velocities of two bodies in an elastic collision, given their masses and initial velocities.
 
-  Args:
-    m1: Mass of the first body.
-    v1: Initial velocity of the first body.
-    m2: Mass of the second body.
-    v2: Initial velocity of the second body.
+    Args:
+        m1: Mass of the first body.
+        v1: Initial velocity of the first body.
+        m2: Mass of the second body.
+        v2: Initial velocity of the second body.
 
-  Returns:
-    A tuple of two floats, representing the final velocities of the first and second bodies, respectively.
-  """
+    Returns:
+        A tuple of two floats, representing the final velocities of the first and second bodies, respectively.
+    """
 
+    # Calculate the final velocity of the first body.
+    v1_final = ((m1 - m2) / (m1 + m2)) * v1 + (2 * m2 / (m1 + m2)) * v2
 
-  # Calculate the final velocity of the first body.
-  v1_final = ((m1 - m2) / (m1 + m2))*v1+(2*m2/(m1+m2))*v2
+    # Calculate the final velocity of the second body.
+    v2_final = (2 * m1 / (m1 + m2)) * v1 + ((m2 - m1) / (m1 + m2)) * v2
 
-  # Calculate the final velocity of the second body.
-  v2_final = ((2*m1) / (m1 + m2))*v1+((m2-m1)/(m1+m2))*v2
-
-  return v1_final, v2_final
+    return v1_final, v2_final
 
 def elastic_collision(masses, velocities, angles):
-  """Calculates the final velocities of two bodies in an elastic collision, given their masses, initial velocities, and angles.
+    """Calculates the final velocities of two bodies in an elastic collision, given their masses, initial velocities, and angles.
 
-  Args:
-    masses: A list or array of the masses of the two bodies.
-    velocities: A list or array of the initial velocities of the two bodies.
-    angles: A list or array of the angles of the initial velocities of the two bodies, in degrees.
+    Args:
+        masses: A list or array of the masses of the two bodies.
+        velocities: A list or array of the initial velocities of the two bodies.
+        angles: A list or array of the angles of the initial velocities of the two bodies, in degrees.
 
-  Returns:
-    A tuple of two lists or arrays, representing the final velocities of the two bodies, respectively.
-  """
+    Returns:
+        A tuple of two lists or arrays, representing the final velocities of the two bodies, respectively.
+    """
 
-  # Convert the angles to radians.
-  angles = np.radians(angles)
+    # Convert the angles to radians.
+    angles = np.radians(angles)
 
-  # Calculate the x- and y-components of the initial velocities.
-  vx1, vy1 = velocities[0] * np.cos(angles[0]), velocities[0] * np.sin(angles[0])
-  vx2, vy2 = velocities[1] * np.cos(angles[1]), velocities[1] * np.sin(angles[1])
+    # Calculate the x- and y-components of the initial velocities using map.
+    initial_vx = list(map(lambda v, angle: v * np.cos(angle), velocities, angles))
+    initial_vy = list(map(lambda v, angle: v * np.sin(angle), velocities, angles))
 
-  # Calculate the final x- and y-components of the velocities.
-  vx1_final, vx2_final = conserve_momentum(masses[0], vx1, masses[1], vx2)
-  vy1_final, vy2_final = conserve_momentum(masses[0], vy1, masses[1], vy2)
+    # Calculate the final x- and y-components of the velocities using conserve_momentum.
+    final_vx = conserve_momentum(masses[0], initial_vx[0], masses[1], initial_vx[1])
+    final_vy = conserve_momentum(masses[0], initial_vy[0], masses[1], initial_vy[1])
 
-  # Calculate the final velocities.
-  velocities_final = [
-      np.sqrt(vx1_final**2 + vy1_final**2),
-      np.sqrt(vx2_final**2 + vy2_final**2)
-  ]
+    # Calculate the final velocities using list comprehension.
+    velocities_final = [np.sqrt(vx ** 2 + vy ** 2) for vx, vy in zip(final_vx, final_vy)]
 
-  # Calculate the final angles.
-  angles_final = [
-      np.arctan2(vy1_final, vx1_final),
-      np.arctan2(vy2_final, vx2_final)
-  ]
+    # Calculate the final angles using list comprehension.
+    angles_final = [np.arctan2(vy, vx) for vx, vy in zip(final_vx, final_vy)]
 
-  # Convert the angles back to degrees.
-  angles_final = np.degrees(angles_final)
+    # Convert the angles back to degrees.
+    angles_final = np.degrees(angles_final)
 
-  return velocities_final, angles_final
+    return velocities_final, angles_final
 
-# Unit Test:
-
+# Example usage:
 masses = [1, 1]
-velocities = [1,0]
+velocities = [0, -1]
 angles = [0, 0]
 
 velocities_final, angles_final = elastic_collision(masses, velocities, angles)
