dc13a3e1b2950d3f55c8bfd82ad2e11aa31f37a5
LinxuanHu <44795054+LinxuanHu@users.noreply.github.com>
2023-10-06 01:26:23 -0400

    new

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,7 @@
 import unittest
 import math
 import numpy as np
+import elastic_collision
 class Particle:
     def __init__(self, mass, velocity):
         self.mass = mass
@@ -17,18 +18,26 @@ class ParticleTest(unittest.TestCase):
 
     def test_momentum(self):
         """Test the momentum calculation."""
-
-        # Create a particle with mass 1 and velocity 1
-        particle = Particle(1, 1)
-
+        masses = [1, 1]
+        velocities = [1, -1]
         # Calculate the expected momentum
-        expected_momentum = 1
+        expected_momentum = 0
 
         # Calculate the actual momentum
-        actual_momentum = particle.momentum()
+        v = elastic_collision.elastic_collision(masses,velocities)
+        v1=v[0]
+        v2=v[1]
+        m1=masses[0]
+        m2=masses[1]
+        actual_momentum=v1*m1+v2*m2
 
         # Assert that the expected and actual momenta are equal
-        self.assertEqual(expected_momentum, actual_momentum)
+        p_conserved=self.assertEqual(expected_momentum, actual_momentum)
+        if p_conserved==None:
+            print('momentum is conserved')
+        else:
+            print('error, momentum is not conserved')
+        return p_conserved
 
     def test_kinetic_energy(self):
         """Test the kinetic energy calculation."""
@@ -43,5 +52,8 @@ class ParticleTest(unittest.TestCase):
         actual_kinetic_energy = particle.kinetic_energy()
 
         # Assert that the expected and actual kinetic energies are equal
-        self.assertEqual(expected_kinetic_energy, actual_kinetic_energy)
+        #self.assertEqual(expected_kinetic_energy, actual_kinetic_energy)
+        print(self.assertEqual(expected_kinetic_energy, actual_kinetic_energy))
 
+if __name__ == '__main__':
+    unittest.main()
